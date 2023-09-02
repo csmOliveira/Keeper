b712fbd2041175e7ed9b681de0774228d869c68c
Carlos Emanuel Silva e Melo Oliveira <carlos.olvr32@gmail.com>
2023-09-02 07:24:00 -0300

    Finalizando projeto

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes.js";
-
-function createNote(note) {
-    return (
-	<Note
-	    id={note.key}
-	    title={note.title}
-	    content={note.content}
-	/>
-    )
-}
+import CreateArea from "./CreateArea";
 
 function App() {
-    return (
-	<div>
-	    <Header />
-	    
-	    {notes.map(createNote)}
+  const [note, setNote] = useState([]);
 
-	    <Footer />
-	</div>
-    );
- }
+  function addItem(noteContent) {
+    setNote((prevValues) => {
+      return [...prevValues, noteContent];
+    });
+  }
+
+  function removeItem(id) {
+    setNote((prevValues) => {
+      return prevValues.filter((itens, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addItem} />
+      {note.map((itens, index) => (
+        <Note
+          key={index}
+          id={index}
+          title={itens.title}
+          content={itens.content}
+          onDelete={removeItem}
+        />
+      ))}
+      <Footer />
+    </div>
+  );
+}
 
 export default App;
